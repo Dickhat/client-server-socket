bc64d1d555b42bb724d80f7a606cc37296365c31
KANEKI <90392757+Dickhat@users.noreply.github.com>
2024-10-31 19:49:46 +0300

    single threaded server

---
+++ ClientSocket/client.py
@@ -0,0 +1,49 @@
+import socket
+import time
+import datetime
+
+sock = socket.socket()
+
+ip = 'empty' 
+port = 0
+
+# Получение данных из файла конфигурации
+with open('.\\configure.txt', 'r') as conf_file:
+    data = conf_file.read().split(':')
+
+    if (data.__len__ != 2):
+        print("\nconfigure file incorrect\n")
+        exit 
+
+    ip = data[0]
+    port = int(data[1])
+
+# Подключение к серверу
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    sock.connect((ip, port))
+
+    file = open('ClientLog.txt', 'a')
+
+    # Время подключения к серверу
+    cur_time = datetime.datetime.today()
+    file.write(f"\n\nConnection {cur_time.day}.{cur_time.month}.{cur_time.year} {cur_time.hour}:{cur_time.minute}:{cur_time.second}")
+    file.write(f"\nServer {ip}:{port}")
+
+    time.sleep(2)
+
+    # Отправка серверу данных
+    send_str = input()
+    sock.sendall(send_str.encode('UTF-8'))
+
+    # Запись времени отправки
+    cur_time = datetime.datetime.today()
+    file.write(f"\nMessage send time {cur_time.hour}:{cur_time.minute}:{cur_time.second}")
+    file.write(f"\nMessage content: {send_str}")
+
+    # Получение ответа от сервера
+    response = sock.recv(4096)
+    cur_time = datetime.datetime.today()
+    file.write(f"\nMessage recieved time {cur_time.hour}:{cur_time.minute}:{cur_time.second}")
+    file.write(f"\nMessage content: {response}")
+
+    file.close()
---
+++ ServerSocket/ServerSocket.cpp
@@ -0,0 +1,134 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+
+int main()
+{
+    // Структура WSADATA содержит сведения о реализации сокетов Windows
+    WSADATA wsaData;                            
+    int WSAError;                            // Код возврата
+    WORD wVersionRequested = MAKEWORD(2, 2); // Версия socket 2.2
+
+    // Инициирует использование Winsock DLL процессом.
+    WSAError = WSAStartup(wVersionRequested, &wsaData);
+    
+    // Проверка, что dll подключилась
+    if (WSAError != 0 )
+    {
+        std::cout << " Error: WSA startup failed\n";
+        return WSAError;
+    }
+
+    // Создание сокета
+    SOCKET serverSocket;
+    serverSocket = INVALID_SOCKET;
+    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    // Проверка создания сокета
+    if (serverSocket == INVALID_SOCKET) {
+        std::cout << "Error at socket: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "Socket is OK!" << std::endl;
+
+    // Bind data
+    sockaddr_in service;                                    // IPv4 Socket address
+    service.sin_family = AF_INET;                           // internetwork: UDP, TCP, etc.
+    service.sin_addr.s_addr = inet_addr("192.168.0.130");   // IP address
+    service.sin_port = htons(30411);                        // Choose a port number
+
+    // Подвязка данных
+    if (bind(serverSocket, (SOCKADDR*) &service, sizeof(service)) == SOCKET_ERROR) {
+        std::cout << "bind failed: " << WSAGetLastError() << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "bind is OK!" << std::endl;
+
+    std::fstream logFile;
+    logFile.open(".\\log.txt", std::ios::app);
+
+    // Время старта сервера
+    time_t timestamp;
+    time(&timestamp);
+    logFile << "\n\nServer start " << ctime(&timestamp);
+
+    while (true)
+    {
+        // Прослушивание входящих подключений
+        if (listen(serverSocket, 5) == SOCKET_ERROR){
+            std::cout << "listen: Error listening on socket: " << WSAGetLastError() << std::endl;
+            continue;
+        }
+
+        std::cout << "listen is OK! I'm waiting for new connections..." << std::endl;
+
+        // Сокет для входящих подключений
+        SOCKET clientSocket;
+        clientSocket = accept(serverSocket, nullptr, nullptr);
+
+        if (clientSocket == INVALID_SOCKET) {
+            std::cout << "accept failed: " << WSAGetLastError() << std::endl;
+        }
+        else {
+            std::cout << "accept is OK!" << std::endl;
+
+            // Время подключения
+            time(&timestamp);
+            logFile << "\n\nConnection time " << ctime(&timestamp);
+
+            char recvbuf[1024];
+
+            for (int i = 0; i < 1024; ++i)
+            {
+                recvbuf[i] = '\0';
+            }
+
+            int byteRecv = 0;
+            int byteSend = 0;
+            // Время получения сообщения
+            time(&timestamp);
+            logFile << "Message recieved time " << ctime(&timestamp) << "Recieved message:";
+
+            std::string response;
+
+            // Получение данных
+            while (true) {
+                byteRecv = recv(clientSocket, recvbuf, 1024, 0);
+
+                response.append(std::string(recvbuf, byteRecv));
+                logFile << recvbuf;
+
+                if (byteRecv < 1024)
+                    break;
+            }
+            
+            // Отзеркалирование сообщения
+            std::reverse(response.begin(), response.end());
+            response.append(" Server written by Bogdanov Daniil Alekseevich M30-411B-21");
+
+            logFile << "\nMessage send time " << ctime(&timestamp) << "Send message:";
+
+            byteSend = send(clientSocket, response.c_str(), strlen(response.c_str()), 0);
+            logFile << response;
+        }
+        
+        logFile << "\n Disconnect time " << ctime(&timestamp);
+        logFile.flush();            // Обнуление буфера для отображения данных в файле
+        closesocket(clientSocket);
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+    return 0;
+}
